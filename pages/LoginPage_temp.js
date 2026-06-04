f9e09ff7a89495a4146fb220e4c07a0eabc5c022
SoumyajitBhat <soumyajit.bhattacharjee@cloudkaptan.com>
2026-06-04 10:16:38 +0530

    temp rename LoginPage

---
+++ pages/LoginPage_temp.js
@@ -0,0 +1,19 @@
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.url = "https://staging.xag.co.nz/";
+
+    // Locators
+    this.passwordInput = page.locator('[type="password"]');
+    this.submitButton = page.locator('[type="submit"]');
+  }
+
+  async navigate() {
+    await this.page.goto(this.url);
+  }
+
+  async login(password) {
+    await this.passwordInput.fill(password);
+    await this.submitButton.click();
+  }
+}
